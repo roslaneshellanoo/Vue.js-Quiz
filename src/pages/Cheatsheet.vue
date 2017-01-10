fca82e90456b0fbea2f64d92bc0e04524f61cadd
roslanelyashiv <roslane@shellanoo.com>
2017-01-10 15:54:49 +0200

    tooltip new

--- src/pages/Cheatsheet.vue
+++ src/pages/Cheatsheet.vue
@@ -1,9 +1,11 @@
 <template>
 <div class="wrap-ch">
+
   <div class="text-center">
     <h1>Javascript Cheat Sheet</h1>
   </div>
   <div class="list-of-items mu-paper mu-paper-round mu-paper-1">
+
     <div class="mu-sub-header">Array Methods</div>
 
     <div v-for="item in items" class="single-item">
@@ -37,7 +39,6 @@
 
 
 
-
 //        reverse()
 //        shift()
 //        slice()
@@ -59,20 +60,20 @@
           },
           {
             text: 'join()',
-            desc: 'The Array.from() method creates a new Array instance from an array-like or iterable object.',
+            desc: 'The join() method joins all elements of an array into a string.',
           },
           {
             text: 'lastIndexOf()',
-            desc: 'The Array.from() method creates a new Array instance from an array-like or iterable object.',
+            desc: 'The lastIndexOf() method returns the last index at which a given element can be found in the array, or -1 if it is not present. The array is searched backwards, starting at fromIndex.',
           },
           {
             text: 'pop()',
-            desc: 'The Array.from() method creates a new Array instance from an array-like or iterable object.',
+            desc: 'The pop() method removes the last element from an array and returns that element. This method changes the length of the array.',
 
           },
           {
             text: 'push()',
-            desc: 'The Array.from() method creates a new Array instance from an array-like or iterable object.',
+            desc: 'The push() method adds one or more elements to the end of an array and returns the new length of the array.',
           },
         ],
 
@@ -107,6 +108,7 @@
 
   .list-of-items .mu-icon {
     display: block;
+    cursor: pointer;
   }
 
   .toggle-list-item {
@@ -120,8 +122,11 @@
     padding: 1.5rem;
     display: block;
     bottom: 4.2rem;
-    right: 17px;
-    border-radius: 7px 7px 0 7px;
+    right: 0;
+    border-radius: 7px;
+    margin: 0 -1rem;
+
+    box-shadow: 0 1px 6px rgba(0, 0, 0, 0.72);
   }
 
   .item-desc:after {
@@ -131,10 +136,11 @@
     bottom: -10px;
     content: " ";
     height: 0;
-    right: 0;
-    margin-left: -13px;
+    right: 29px;
+    margin-left: 0;
     position: absolute;
     width: 0;
   }
+
 
 </style>
