b505f80de3b3b054585c4d75cf8751b809f4257f
roslanelyashiv <roslane@shellanoo.com>
2017-01-14 17:38:20 +0200

    es6 import

--- src/api/cheatsheet.js
+++ src/api/cheatsheet.js
@@ -10,6 +10,60 @@ const Items = [
 
     },
 
+  {
+    text: 'join()',
+    desc: 'The join() method joins all elements of an array into a string.',
+  },
+  {
+    text: 'lastIndexOf()',
+    desc: 'The lastIndexOf() method returns the last index at which a given element can be found in the array, or -1 if it is not present. The array is searched backwards, starting at fromIndex.',
+
+  },
+  {
+    text: 'pop()',
+    desc: 'The pop() method removes the last element from an array and returns that element. This method changes the length of the array.',
+  },
+  {
+    text: 'push()',
+    desc: 'The push() method adds one or more elements to the end of an array and returns the new length of the array.',
+
+  },
+  {
+    text: 'reverse()',
+    desc: 'The reverse() method reverses an array in place. The first array element becomes the last, and the last array element becomes the first.',
+  },
+  {
+    text: 'shift()',
+    desc: 'The shift() method removes the first element from an array and returns that element. This method changes the length of the array.',
+
+  },
+  {
+    text: 'slice()',
+    desc: 'The slice() method returns a shallow copy of a portion of an array into a new array object selected from begin to end (end not included). The original array will not be modified.',
+  },
+  {
+    text: 'sort()',
+    desc: 'The sort() method sorts the elements of an array in place and returns the array.',
+
+  },
+  {
+    text: 'splice()',
+    desc: 'The splice() method changes the content of an array by removing existing elements and/or adding new elements.',
+  },
+  {
+    text: 'toString()',
+    desc: 'The toString() method returns a string representing the specified array and its elements.',
+
+  },
+  {
+    text: 'unshift()',
+    desc: 'The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.',
+  },
+  {
+    text: 'valueOf()',
+    desc: 'The valueOf() method returns the primitive value of the specified object.',
+
+  },
 
 
 
@@ -37,4 +91,5 @@ export  {
   Items,
   Apples
 }
+
 
