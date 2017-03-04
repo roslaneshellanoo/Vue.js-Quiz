5ea30095f0a54fa826d48952e92c79af6e8efc84
roslanelyashiv <roslane@shellanoo.com>
2017-03-04 23:32:20 +0200

    sidebar removed

--- src/Navigation/SidebarContent.vue
+++ src/Navigation/SidebarContent.vue
@@ -21,33 +21,6 @@
 
     <mu-divider/>
 
-    <mu-list>
-      <mu-sub-header>Code Quizes</mu-sub-header>
-      <mu-list-item title="Javascript Quiz" toggleNested :open="opennav">
-        <mu-list-item slot="nested" title="Jquery"></mu-list-item>
-        <mu-list-item slot="nested" title="React.js"></mu-list-item>
-        <mu-list-item slot="nested" title="Vue.js"></mu-list-item>
-      </mu-list-item>
-
-      <mu-list-item title="Python Quiz" toggleNested :open="opennav">
-        <mu-list-item slot="nested" title="Jquery"></mu-list-item>
-        <mu-list-item slot="nested" title="React.js"></mu-list-item>
-        <mu-list-item slot="nested" title="Vue.js"></mu-list-item>
-      </mu-list-item>
-
-    </mu-list>
-
-    <mu-divider/>
-
-    <mu-list>
-      <mu-sub-header>Cheatsheets</mu-sub-header>
-
-      <mu-list-item title="Jquery"></mu-list-item>
-      <mu-list-item title="React.js"></mu-list-item>
-      <mu-list-item title="Vue.js"></mu-list-item>
-
-
-    </mu-list>
 
   </div>
 
